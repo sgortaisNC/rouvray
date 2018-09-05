128a85d4310b201dce3f1a555407529704f27e84
Sébastien Gortais <sgortais@net-com.fr>
2018-09-05 15:58:13 +0200

    100% - Modification Jennyfer

--- modules/custom/nc_resize/js/resize.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-    $('.resize').on('click', function(){
-        if($(this).hasClass('resize-plus')){
-            $('body').css('font-size', parseInt($('body').css('font-size')) + 1);
-        }else{
-            $('body').css('font-size', parseInt($('body').css('font-size')) - 1);
-        }
-    });
-});
