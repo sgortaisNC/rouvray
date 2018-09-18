e65bd6ca1b7e3636173c8e8bb39f8f2c1ed9475f
Sébastien Gortais <sgortais@net-com.fr>
2018-09-18 13:56:50 +0200

    Ajout templates + hook + libraries

--- themes/site/inc/hook_nodes.php
+++ themes/site/inc/hook_nodes.php
@@ -1,1 +1,28 @@
 <?php
+
+use Drupal\file\Entity\File;
+use Drupal\image\Entity\ImageStyle;
+
+
+/**
+ * Implements hook_preprocess_HOOK().
+ */
+function site_preprocess_node( &$variables ) {
+	//$node = \Drupal::routeMatch()->getParameter('node');
+	$node = $variables['node'];
+	if ( $node instanceof \Drupal\node\NodeInterface ) {
+		if ( $node->bundle() == "gallerie" ) {
+			$gallerie = $node->get( 'field_images' )->getValue();
+			$variables["gallerie"] = [];
+			foreach ( $gallerie as $photo ) {
+				$file = File::load( $photo['target_id'] );
+				if ( ! empty( $file ) ) {
+					$path     = $file->getFileUri();
+					$urlImage = file_create_url( ImageStyle::load( 'large' )->buildUrl( $path ) );
+					$altImage = $photo['alt'];
+					$variables["gallerie"][] = ["url" => $urlImage, "alt" => $altImage];
+				}
+			}
+		}
+	}
+}
